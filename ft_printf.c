547efd101d6f06b27bc34da2b442e660768de3d6
cperales-42 <cperalessan@gmail.com>
2024-12-11 13:32:22 +0100

    Done

--- ft_printf.c
+++ ft_printf.c
@@ -3,50 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jgomez-d <jgomez-d@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cperales <cperales@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/02 01:15:38 by jorge             #+#    #+#             */
-/*   Updated: 2024/12/04 22:47:34 by jgomez-d         ###   ########.fr       */
+/*   Created: 2024/12/11 13:29:25 by cperales          #+#    #+#             */
+/*   Updated: 2024/12/11 13:29:33 by cperales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-//este es nuestro codigo
-
-/*void	ft_print_pf(char *fmt, int *counter, va_list arglst)
+void	ft_putchar_pf(char c, int *counter)
 {
-	if (*fmt == 'c' || *fmt == '%')
-		ft_putchar_pf(va_arg(arglst, int), counter);
-	else if (*fmt == 's')
-		ft_putstr_pf(va_arg(arglst, char *), counter);
-	else if (*fmt == 'i' || *fmt == 'd')
-		ft_putnbr_pf(va_arg(arglst, int), counter);
-	else if (*fmt == 'u')
-		ft_putu_pf(va_arg(arglst, unsigned int), counter);
-	else if (*fmt == 'x' || *fmt == 'X')
-		ft_puthex_pf(va_arg(arglst, unsigned long), counter, *fmt);
-	else if (*fmt == 'p')
-		ft_putptr(va_arg(arglst, void *), counter);
+	write(1, &c, 1);
+	(*counter)++;
 }
-
-int	ft_printf(const char *format, ...)
-{
-	va_list	arglst;
-	int		counter;
-	char	*fmt;
-
-	fmt = (counter = 0, (char *)format);
-	fmt--;
-	va_start(arglst, format);
-	while (*(++fmt))
- 		if (*fmt++ != '%')
-			ft_putchar_pf(*fmt, &counter);
-		else 
-			ft_print_pf(fmt, &counter, arglst);
-	return (va_end(arglst), counter);
-}*/
-
-//este chatgpt
 
 void	ft_print_pf(char *fmt, int *counter, va_list arglst)
 {
